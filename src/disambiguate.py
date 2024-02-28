28148671e880aa95ac163c8e83769344698e400b
Anabella Isaro <anabella.isaro@tylertech.com>
2024-02-28 16:00:41 -0600

    clean up

--- src/disambiguate.py
+++ src/disambiguate.py
@@ -1,24 +1,13 @@
 import requests
 from typing import List
 import json
-
-endpointUrl = 'https://query.wikidata.org/sparql'
-version = '0.1.0'
-created = '2023-02-22'
-user_agent_header = 'VanderSearchBot/' + version + ' (https://github.com/HeardLibrary/linked-data/tree/master/vanderbot; mailto:steve.baskauf@vanderbilt.edu)'
-request_header_dictionary = {
-	'Accept' : 'application/json',
-	'User-Agent': user_agent_header
-}
-search_session = requests.Session()
-search_session.headers.update(request_header_dictionary)
+from SPARQLWrapper import SPARQLWrapper
 
 class Disambiguate():
     def __init__(self):
         return
 
-    #This section was 
-    def search_cirrus(self, search_string: str, http: requests.Session, api_endpoint='https://www.wikidata.org/w/api.php') -> List[dict]:
+    def search_cirrus(self, search_string: str, api_endpoint='https://www.wikidata.org/w/api.php') -> List[dict]:
 
         request_string = '''{
             "action": "query",
@@ -28,7 +17,7 @@ class Disambiguate():
             "srsearch": "''' + search_string + '''"
         }'''
 
-        response = http.get(api_endpoint, params=json.loads(request_string))
+        response = requests.get(api_endpoint, params=json.loads(request_string))
         data = response.json()
 
         hits = data['query']['search']
@@ -41,9 +30,8 @@ class Disambiguate():
                 "ids": "''' + hit['title'] + '''",
                 "props": "labels"
             }'''
-            response = http.get(api_endpoint, params=json.loads(request_string))
+            response = requests.get(api_endpoint, params=json.loads(request_string))
             data = response.json()
-            #print(json.dumps(data, indent=2))
 
             # Match the Q ID to the label and add the label to the hits list
             try:
@@ -70,20 +58,39 @@ class Disambiguate():
     
     def getCollectAnswerQid(self, input_question):
         multiple_question = {}
+        returned_qid = []
         i = 1
         print("- Multiple Items came up when you searched '", input_question, ", Did you mean:")
-        hits = self.search_cirrus(input_question, search_session)
+        hits = self.search_cirrus(input_question)
         # print(json.dumps(hits, indent=2))
         for entry in hits:
             multiple_question[i] = [entry.get("description"), entry.get("qid")]
             print( "-", i,  entry.get("description"))
+            returned_qid.append(entry.get("qid"))
             i = i + 1
         # Taking user input and converting it to an integer
         user_input = int(input("Enter the corresponding number you mean: "))
         print('The user input is:', user_input )
         answer = multiple_question.get(user_input)
         print("The selected answer:", answer[0], "and the Qid:", answer[1])
-        # return answer
+        return answer[1]
+    
+    def construct_query(self, returned_qid):
+        # for item in returned_qid:
+        query = '''
+        CONSTRUCT {
+        wd:''' + returned_qid + ''' ?p1 ?o.
+        ?s ?p2 wd:''' + returned_qid + '''.
+        }
+        WHERE {
+        {wd:''' + returned_qid + ''' ?p1 ?o.}
+        UNION
+        {?s ?p2 wd:''' + returned_qid + '''.}
+        }
+        '''
+        print(query)
 if __name__ == "__main__":
+    
     disambiguate = Disambiguate()
-    disambiguate.getCollectAnswerQid('Geese')
+    returned_qid = disambiguate.getCollectAnswerQid('politician')
+    disambiguate.construct_query(returned_qid)
